48f9b36898c4d2517ec621ab90d74adc9096cc89
anthonyting <49772744+anthonyting@users.noreply.github.com>
2025-10-22 11:17:01 -0700

    feat(examples): add more tests and assertions (#143)
    *Issue #, if available:*
    
    *Description of changes:*
    
    First PR for moving tests from testing library integration tests to
    examples. This will allow them to be run on the backend.
    
    This PR is primarily moving tests in
    `local-durable-test-runner.integration.test.ts` from the testing
    library.
    
    Also adding more assertions in some tests.
    
    TODO:
    - Move more tests
    - Write more tests
    
    
    By submitting this pull request, I confirm that you can use, modify,
    copy, and redistribute this contribution, under the terms of your
    choice.

--- packages/aws-durable-execution-sdk-js-examples/src/examples/__tests__/hello-world.test.ts
+++ packages/aws-durable-execution-sdk-js-examples/src/examples/__tests__/hello-world.test.ts
@@ -6,10 +6,11 @@ createTests({
   functionName: "hello-world",
   handler,
   tests: (runner) => {
-    it("should return as expected", async () => {
+    it("should return as expected with no operations", async () => {
       const execution = await runner.run();
 
       expect(execution.getResult()).toEqual("Hello World!");
+      expect(execution.getOperations()).toHaveLength(0);
     });
   },
 });
