d6b71d496c676199b1ea4f87b176d8094d86340b
hsilan <hsilan@amazon.com>
2026-04-17 14:23:22 -0700

    chore: mark dependencies as external

--- packages/aws-durable-execution-sdk-js-otel/rollup.config.mjs
+++ packages/aws-durable-execution-sdk-js-otel/rollup.config.mjs
@@ -3,8 +3,16 @@
 import { createBuildOptions } from "../../rollup.config.js";
 import packageJson from "./package.json" with { type: "json" };
 
+// Mark all dependencies and peer dependencies as external to avoid bundling them
+const external = [
+  ...Object.keys(packageJson.dependencies || {}),
+  ...Object.keys(packageJson.peerDependencies || {}),
+];
+
 const config = {
   input: "./src/index.ts",
+  external: (id) =>
+    external.some((dep) => id === dep || id.startsWith(dep + "/")),
   output: {
     file: "index",
     inlineDynamicImports: true,
