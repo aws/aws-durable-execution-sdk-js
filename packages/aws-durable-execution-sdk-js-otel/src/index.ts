a5be3ae0679263c74270459173a414561bfbb375
hsilan <hsilan@amazon.com>
2026-05-20 11:16:13 -0700

    chore: remove otel plugin, test otel package

--- packages/aws-durable-execution-sdk-js-otel/src/index.ts
+++
@@ -1,8 +0,0 @@
-export { DurableOtelPlugin } from "./durable-otel-plugin";
-export type { DurableOtelPluginConfig } from "./durable-otel-plugin";
-export { DeterministicIdGenerator } from "./deterministic-id-generator";
-export {
-  xRayContextExtractor,
-  w3cClientContextExtractor,
-} from "./context-extractors";
-export type { ContextExtractor } from "./context-extractors";
